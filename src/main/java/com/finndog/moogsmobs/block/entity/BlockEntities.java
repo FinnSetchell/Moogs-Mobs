a13941dba027d220ffd923bf74a8e1b541ecde0a
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-11-13 17:46:28 +0000

    removed JAR and GLOWBUG JAR

--- src/main/java/com/finndog/moogsmobs/block/entity/BlockEntities.java
+++ src/main/java/com/finndog/moogsmobs/block/entity/BlockEntities.java
@@ -13,19 +13,6 @@ public class BlockEntities {
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, MoogsMobs.MODID);
 
-//    public static final RegistryObject<BlockEntityType<AnimatedJarBlockEntity>> ANIMATED_JAR_BLOCK_ENTITY =
-//            BLOCK_ENTITIES.register("animated_glowbug_jar_block", () ->
-//                    BlockEntityType.Builder.of(AnimatedJarBlockEntity::new,
-//                            ModBlocks.ANIMATED_JAR_BLOCK.get()).build(null));
-
-    public static final RegistryObject<BlockEntityType<JarEntity>> JAR_ENTITY =
-            BLOCK_ENTITIES.register("jar_entity",
-                    () -> BlockEntityType.Builder.of(JarEntity::new, ModBlocks.JAR.get()).build(null));
-
-        public static final RegistryObject<BlockEntityType<GlowbugJarEntity>> GLOWBUG_JAR_ENTITY =
-            BLOCK_ENTITIES.register("glowbug_ar_entity",
-                    () -> BlockEntityType.Builder.of(GlowbugJarEntity::new, ModBlocks.GLOWBUG_JAR.get()).build(null));
-
     public static final RegistryObject<BlockEntityType<KegBlockEntity>> KEG_ENTITY =
             BLOCK_ENTITIES.register("keg_entity",
                     () -> BlockEntityType.Builder.of(KegBlockEntity::new, ModBlocks.KEG.get()).build(null));
