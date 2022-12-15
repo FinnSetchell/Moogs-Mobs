1fd5fa8f9516647def1b88290eef3c1557fc3be8
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2022-12-15 19:46:46 +0000

    added stone cutter recipes

--- src/main/java/com/finndog/moogsmobs/datagen/ModRecipeProvider.java
+++ src/main/java/com/finndog/moogsmobs/datagen/ModRecipeProvider.java
@@ -4,10 +4,11 @@ import com.finndog.moogsmobs.block.ModBlocks;
 import com.finndog.moogsmobs.item.ModItems;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SmithingTableBlock;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 
@@ -21,20 +22,34 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     @Override
     protected void buildCraftingRecipes(Consumer<FinishedRecipe> pFinishedRecipeConsumer) {
 
-//        ShapedRecipeBuilder.shaped(ModBlocks.EBONY_DOOR.get())
-//                .define('E', ModBlocks.EBONY_PLANKS.get())
-//                .pattern("EE")
-//                .pattern("EE")
-//                .pattern("EE")
-//                .unlockedBy("has_ebony_planks", inventoryTrigger(ItemPredicate.Builder.item()
-//                        .of(ModBlocks.EBONY_PLANKS.get()).build()))
-//                .save(pFinishedRecipeConsumer);
-//
-//        ShapelessRecipeBuilder.shapeless(ModItems.CITRINE.get())
-//                .requires(ModBlocks.CITRINE_BLOCK.get())
-//                .unlockedBy("has_citrine_block", inventoryTrigger(ItemPredicate.Builder.item()
-//                        .of(ModBlocks.CITRINE_BLOCK.get()).build()))
-//                .save(pFinishedRecipeConsumer);
+        smithingRecipe(Blocks.POLISHED_DEEPSLATE.asItem(), Items.COPPER_INGOT, ModBlocks.POLISHED_COPPER_DEEPSLATE.get().asItem())
+                .unlocks("has_copper_ingot", has(Items.COPPER_INGOT))
+                .save(pFinishedRecipeConsumer, getItemName(ModBlocks.POLISHED_COPPER_DEEPSLATE.get().asItem()) + "_smithing");
+        smithingRecipe(Blocks.POLISHED_DEEPSLATE.asItem(), Items.GOLD_INGOT, ModBlocks.GILDED_POLISHED_DEEPSLATE.get().asItem())
+                .unlocks("has_copper_ingot", has(Items.GOLD_INGOT))
+                .save(pFinishedRecipeConsumer, getItemName(ModBlocks.GILDED_POLISHED_DEEPSLATE.get().asItem()) + "_smithing");
+        smithingRecipe(Blocks.POLISHED_DEEPSLATE.asItem(), ModItems.BRASS_INGOT.get(), ModBlocks.BRASS_POLISHED_DEEPSLATE.get().asItem())
+                .unlocks("has_copper_ingot", has(ModItems.BRASS_INGOT.get()))
+                .save(pFinishedRecipeConsumer, getItemName(ModBlocks.BRASS_POLISHED_DEEPSLATE.get().asItem()) + "_smithing");
 
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.COPPER_DEESPLATE_PILLAR.get(), ModBlocks.POLISHED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.EXPOSED_COPPER_DEEPSLATE_PILLAR.get(), ModBlocks.POLISHED_EXPOSED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.WEATHERED_COPPER_DEEPSLATE_PILLAR.get(), ModBlocks.POLISHED_WEATHERED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.OXIDIZED_COPPER_DEEPSLATE_PILLAR.get(), ModBlocks.POLISHED_OXIDIZED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.GILDED_DEEPSLATE_PILLAR.get(), ModBlocks.GILDED_POLISHED_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.RUNIC_GILDED_DEEPSLATE_PILLAR.get(), ModBlocks.GILDED_POLISHED_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.BRASS_DEEPSLATE_PILLAR.get(), ModBlocks.BRASS_POLISHED_DEEPSLATE.get());
+
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.COPPER_DEEPSLATE_TILES.get(), ModBlocks.POLISHED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.EXPOSED_COPPER_DEEPSLATE_TILES.get(), ModBlocks.POLISHED_EXPOSED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.WEATHERED_COPPER_DEEPSLATE_TILES.get(), ModBlocks.POLISHED_WEATHERED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.OXIDIZED_COPPER_DEEPSLATE_TILES.get(), ModBlocks.POLISHED_OXIDIZED_COPPER_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.GILDED_DEEPSLATE_TILES.get(), ModBlocks.GILDED_POLISHED_DEEPSLATE.get());
+        stonecutterResultFromBase(pFinishedRecipeConsumer, ModBlocks.BRASS_DEEPSLATE_TILES.get(), ModBlocks.BRASS_POLISHED_DEEPSLATE.get());
+
+    }
+
+    private UpgradeRecipeBuilder smithingRecipe(Item item1, Item item2, Item result) {
+        return UpgradeRecipeBuilder.smithing(Ingredient.of(item1), Ingredient.of(item2), result);
     }
 }
